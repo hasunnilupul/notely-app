df323bab61db55c52d7fcc6af6a25b8753e311ad
Hasun Nilupul <hasunnilupul16@gmail.com>
2024-10-06 18:12:16 +0530

    added home page link to navbar logo

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 import ProfileInfo from "../Cards/ProfileInfo";
 import SearchBar from "../SearchBar/SearchBar";
@@ -13,7 +13,9 @@ const Navbar = () => {
 
   return (
     <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
-      <h2 className="text-xl font-medium text-black py-2">Notely</h2>
+      <Link to="/">
+        <h2 className="text-xl font-medium text-black py-2">Notely</h2>
+      </Link>
 
       {/* Search bar */}
       <SearchBar
